bda5c3981a0788b92dcc039c12cbd7f9afff2a9b
Adam <adamjseidman@gmail.com>
2022-02-09 19:05:06 -0500

    Added more words and formatted

---
+++ load.js
@@ -0,0 +1,5 @@
+var index = Math.floor((new Date().getTime() - new Date("02/07/2022").getTime()) / (1000 * 3600 * 24));
+
+document.getElementById("tomorrow").innerHTML = words[index + 1].toUpperCase();
+document.getElementById("today").innerHTML = words[index].toUpperCase();
+document.getElementById("yesterday").innerHTML = words[index - 1].toUpperCase();
