d4a641ea36d8411b48ad37e933ed9bc2c860b063
Adam <adamjseidman@gmail.com>
2022-02-16 10:27:45 -0500

    Update again to reflect entire NY-Times list

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-var index = Math.floor((new Date().getTime() - new Date("02/06/2022").getTime()) / (1000 * 3600 * 24));
+var index = Math.floor((new Date().getTime() - new Date("02/14/2022").getTime()) / (1000 * 3600 * 24));
 
 document.getElementById("tomorrow").innerHTML = words[index + 1].toUpperCase();
 document.getElementById("today").innerHTML = words[index].toUpperCase();
